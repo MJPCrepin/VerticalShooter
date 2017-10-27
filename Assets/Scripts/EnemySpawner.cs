7116f257a4d6f605515ba043910c1f2a8237672c
MJPCrepin <manuel@crep.in>
2017-10-27 11:32:26 +0100

    Added dynamic enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public Transform camera;
+    private float cameraDistance;
+
+    // Arrays containing platforms with enemies on them
+    public GameObject[] enemiesLeft = new GameObject[3];
+    public GameObject[] enemiesRight = new GameObject[3];
+    private int smallestArraySize;
+
+    // Keep track of generated platforms
+    Queue<GameObject> platforms = new Queue<GameObject>();
+    private float highestPlatLeft, highestPlatRight;
+    private float nominalPlatformInterval = 10f;
+
+    void Start ()
+    {
+        // Avoids indexOutOfBounds exception if enemies arrays of different length
+        smallestArraySize = Mathf.Min(enemiesLeft.Length, enemiesRight.Length);
+
+        // Used to spawn enemies close to player (away from camera)
+        cameraDistance = 0 - camera.position.z;
+
+        // Initial enemy platform spawns
+        GenerateRandomPlatform(true, 10f); //left
+        GenerateRandomPlatform(false, 10f); //right
+    }
+	
+	void Update () {
+
+        // Generate new left/right platform if player is reaching the top one
+        if (camera.position.y > highestPlatLeft - nominalPlatformInterval)
+        {
+            GenerateRandomPlatform(true, highestPlatLeft+nominalPlatformInterval);
+        }
+        if (camera.position.y > highestPlatRight - nominalPlatformInterval)
+        {
+            GenerateRandomPlatform(false, highestPlatRight+nominalPlatformInterval);
+        }
+
+        // Delete overtaken platforms
+        if (camera.position.y > (platforms.Peek().transform.position.y + 10))
+        {
+            Destroy(platforms.Dequeue());
+        }
+	}
+
+    private void GenerateRandomPlatform(bool leftSide, float nominalVerticalDistance)
+    {
+        // Randomly select a platform from array
+        int randPlatNum = Mathf.FloorToInt(Random.Range(0, smallestArraySize)); 
+
+        // Noise to avoid platforms always lining up (boring)
+        int randVertNoise = Mathf.FloorToInt(Random.Range(-3f, 3f)); 
+        float actualVertDist = nominalVerticalDistance + randVertNoise;
+
+        // Place enemy left or right according to bool
+        float xPosition = -5; // default initialised left
+        switch (leftSide) 
+        {
+            case true: xPosition = -5f; break;
+            case false: xPosition = 5f; break;
+        }
+
+        // New platform position
+        Vector3 newPosition = new Vector3(xPosition, actualVertDist, cameraDistance);
+
+        // Spawn left or right asset based on position
+        switch ((int)xPosition)
+        {
+            case -5: highestPlatLeft = actualVertDist; // Save y pos as ref for next left platform
+                InstantiatePlatform(enemiesLeft[randPlatNum], newPosition, enemiesLeft[randPlatNum].transform.localRotation); break;
+            case 5: highestPlatRight = actualVertDist; // Save y pos as ref for next right platform
+                InstantiatePlatform(enemiesRight[randPlatNum], newPosition, enemiesRight[randPlatNum].transform.localRotation); break;
+        }
+    }
+
+    // Separated to easily generate initial/special platforms
+    private void InstantiatePlatform(GameObject platform, Vector3 newPosition, Quaternion identity)
+    {
+        GameObject newPlat = Instantiate(platform, newPosition, identity);
+        platforms.Enqueue(newPlat);
+    }
+
+}
--- README.md
+++ README.md
@@ -2,16 +2,14 @@ VertShooterDemo
 ====
 Manuel Crepin (manuel@crep.in), Oct'17
 
-Initial commit
----
-
 ## Current features
 * Player object lifts off with any key
 * Camera follows player max height
 * Score based on peak height
 * Floor deleted after liftoff
+* Enemies spawn continuously on platforms
 
 ## To do
-* Create plaforms for enemies dynamically
 * Autoaim for player and enemies
 * Fancy effects time allowing
+* Some form of punishment for just going up?
